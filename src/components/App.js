1ec1d3a869fc25d5efab7f12ae2b1e61527fe4c2
Rico Hancock <richardl@hancock1.com>
2020-01-10 15:38:27 -0700

    Completed react-3-afternoon app. Included search function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
+
 
 class App extends Component {
   constructor() {
@@ -16,35 +18,101 @@ class App extends Component {
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
+    this.searchPosts = this.searchPosts.bind(this)
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then(res => {
+      console.log('Got posts')
+      this.setState({posts: res.data})
+    })
+    .catch(err => {
+      console.log(`Failed to get posts. Error code: ${err}`)
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then(res => {
+      console.log('Got update posts')
+      this.setState({posts: res.data})
+    })
+    .catch( err => {
+      console.log(`Failed to update post. Error code: ${err}`)
+    })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then(res => {
+      console.log('Deleted post')
+      this.setState({posts: res.data})
+    })
+    .catch(err => {
+      console.log(`Failed to delete post. ${err}`)
+    })
   }
 
-  createPost() {
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, { text })
+    .then(res => {
+      console.log('Successfully created post')
+      this.setState({posts: res.data})
+    })
+    .catch(err => {
+      console.log(`Failed to create post. ${err}`)
+    })
+  }
 
+  searchPosts(str){
+    if(str.length > 0){
+      axios.get(`https://practiceapi.devmountain.com/api/posts/filter?text=${str}`)
+      .then(res => {
+        console.log('Filtered posts');
+        this.setState({posts: res.data})
+      })
+      .catch(err => {
+        console.log(`Failed to filter posts. ${err}`)
+      })
+    } else {
+      axios.get(`https://practiceapi.devmountain.com/api/posts`)
+      .then(res => {
+        console.log('Cleared filtered posts');
+        this.setState({posts: res.data})
+      })
+      .catch(err => {
+        console.log(`Failed to clear filtered posts. ${err}`)
+      })
+    }
   }
 
   render() {
     const { posts } = this.state;
-
+    console.log(posts)
     return (
       <div className="App__parent">
-        <Header />
+        <Header searchPostsFn={this.searchPosts}/>
 
         <section className="App__content">
 
-          <Compose />
+          <Compose 
+            createPostFn={ this.createPost }
+          />
           
+          {
+            posts.map(post => (
+              <Post 
+                key={post.id} 
+                text={post.text} 
+                date={post.date} 
+                id={post.id}
+                updatePostFn={this.updatePost}
+                deletePostFn={this.deletePost}
+              />
+            ))
+          }
+
         </section>
       </div>
     );
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,12 +7,29 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor(){
+    super()
+
+    this.state={
+      input: ''
+    }
+  }
+
+  handleInput(val){
+    this.setState({input: val})
+    this.props.searchPostsFn(val)
+  }
+
+  search(){
+    
+  }
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input value={this.state.input} placeholder="Search Your Feed" onChange={e => this.handleInput(e.target.value)}/>
 
           <SearchIcon id="Search__icon" />
         </div>
